b523c0fb869844778975e148a928906bea175fc3
Torroledo <ivan.torroledo@gmail.com>
2018-10-08 16:05:19 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for the talk: creating next generation writers using AI',
+    author='Ivan Torroledo',
+    license='MIT',
+)
